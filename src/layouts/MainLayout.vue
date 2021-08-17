45ad51263e2dc1b2a01c1bd7c2336ea4c38ebb3d
Sergio Morales Rodríguez <sergiogmr@icloud.com>
2021-08-17 09:14:51 +0100

    :tada:

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,55 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header class="bg-primary text-white" elevated>
+      <q-toolbar>
+        <q-btn dense flat icon="menu" round @click="toggleLeftDrawer"/>
+
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          WeatherApp
+        </q-toolbar-title>
+
+        <q-btn dense flat icon="menu" round @click="toggleRightDrawer"/>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" behavior="desktop" elevated overlay side="left">
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-drawer v-model="rightDrawerOpen" behavior="desktop" elevated overlay side="right">
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container>
+      <router-view/>
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    const leftDrawerOpen = ref(false)
+    const rightDrawerOpen = ref(false)
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      },
+
+      rightDrawerOpen,
+      toggleRightDrawer () {
+        rightDrawerOpen.value = !rightDrawerOpen.value
+      }
+    }
+  }
+}
+</script>
